32758dd0fe91da623bb9b7b63396b7700eee2e14
GuilhermeGM21 <91313065+GuilhermeGM21@users.noreply.github.com>
2021-10-09 08:47:42 -0300

    Add files via upload

---
+++ whatsappclone/src/components/ChatIntro.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ChatIntro.css';
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimagensemoldes.com.br%2Fwp-content%2Fuploads%2F2020%2F04%2FS%25C3%25ADmbolo-Whatsapp-PNG.png&f=1&nofb=1" alt=""/>
+            <h1>Mantenha seu celular conectado</h1>
+            <h2>O WhatSapp 2 conecta ao seu telefone para sincronizar suas mensagens.<br/>Para reduzir o uso de dados, conecte seu telefone a uma rede WiFi.</h2>
+        </div>
+    )
+}
---
+++ whatsappclone/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "APIKEY",
+    authDomain: "AUTHDOMAIN",
+    projectId: "PROJECTID",
+    storageBucket: "STORAGEBUCKT",
+    messagingSenderId: "MESSAGINSENDID",
+    appId: "APPID"
+  };
